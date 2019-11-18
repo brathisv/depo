2a2b702c9101dfde99e11720be596a085b69ddd5
Bharathi S <bharathi.singaravelu@gmail.com>
2019-11-18 11:03:28 +0530

    initial spring setup

---
+++ spring-demo/src/main/java/com/brathisv/spring/DrawingApplication.java
@@ -0,0 +1,10 @@
+package com.brathisv.spring;
+
+public class DrawingApplication {
+
+	public static void main(String[] args) {
+		Triangle shape = new Triangle();
+		shape.draw();
+	}
+
+}
---
+++ spring-demo/src/main/java/com/brathisv/spring/Triangle.java
@@ -0,0 +1,8 @@
+package com.brathisv.spring;
+
+public class Triangle {
+
+	public void draw() {
+		System.out.println("This is triangle");
+	}
+}
